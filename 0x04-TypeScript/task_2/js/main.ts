51ab38018d9dd555300aa5410baf243425ba18fb
ifedips20s <ifedips20s@gmail.com>
2025-11-06 00:16:53 +0100

    Add main file

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,12 +1,12 @@
 // ----------------------------
 // Type predicate to check if employee is Director
-function isDirector(employee: Director | Teacher): employee is Director {
+export function isDirector(employee: Director | Teacher): employee is Director {
   return (employee as Director).workDirectorTasks !== undefined;
 }
 
 // ----------------------------
 // Execute work based on employee type
-function executeWork(employee: Director | Teacher): string {
+export function executeWork(employee: Director | Teacher): string {
   if (isDirector(employee)) {
     return employee.workDirectorTasks();
   } else {
@@ -20,3 +20,4 @@ const emp1 = createEmployee(200);
 const emp2 = createEmployee(1000);
 
 console.log(executeWork(emp1)); // Getting to work
+console.log(executeWork(emp2)); // Getting to director tasks
