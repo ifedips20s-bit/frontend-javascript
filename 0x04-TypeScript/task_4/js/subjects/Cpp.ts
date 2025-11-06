71669d64faa7b4bc1ae900d965ed54c8ad518691
ifedips20s <ifedips20s@gmail.com>
2025-11-06 09:42:49 +0100

    Add task file

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,19 @@
+namespace Subjects {
+  // Declaration merging to add new property
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Cpp";
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher.experienceTeachingC || this.teacher.experienceTeachingC <= 0) {
+        return "No available teacher";
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,19 @@
+namespace Subjects {
+  // Declaration merging to add new property
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class React extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for React";
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher.experienceTeachingReact || this.teacher.experienceTeachingReact <= 0) {
+        return "No available teacher";
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
